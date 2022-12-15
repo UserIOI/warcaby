5ad58b2ae7e16ae3d2443d8276fdb9c38b256427
UserIOI <paulharaburda@gmail.com>
2022-12-15 10:34:04 +0100

    jakies poprawki

--- src/main/java/warcaby/App.java
+++ src/main/java/warcaby/App.java
@@ -3,7 +3,6 @@ package warcaby;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.BorderPane;
 
 
